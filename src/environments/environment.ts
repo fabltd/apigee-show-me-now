9329337d9959f6d2eddf9f97cf6c9df00e946eb7
Jeremy <fabltd@users.noreply.github.com>
2024-01-19 12:30:29 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
-  recaptcha: '6LfEZzAkAAAAAAmHwA6WsTFtW4yHljx7uDfs22QT',
-  url: 'http://localhost:8080/v1/',
+  recaptcha: '',
+  url: '',
   firebaseConfig : {
-    apiKey: "AIzaSyBSz00WVTQX6apvVPyzFe-3uY7id700Wq0",
-    authDomain: "qwiklabs-gcp-03-2a13b9bc2c16.firebaseapp.com",
-    projectId: "qwiklabs-gcp-03-2a13b9bc2c16",
-    storageBucket: "qwiklabs-gcp-03-2a13b9bc2c16.appspot.com",
-    messagingSenderId: "997243225397",
-    appId: "1:997243225397:web:3cacb49a4c6027c4f1787c"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   },
-  key: 'CbEhGA7Sl1LiBCNM1i0MHqBBjQMaZtzdSlCm2PAFFlTqEupw'
+  key: ''
 };
